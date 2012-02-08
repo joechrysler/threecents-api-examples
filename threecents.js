5c9bb5a091c62f3725dc158f7765cfb5111b8850
Eric Shull <eric.shull@gmail.com>
2012-02-08 07:21:48 -0500

    JS grabs content of textarea as feedback.

--- threecents.js
+++ threecents.js
@@ -1,20 +1,12 @@
-// To use threecents.js, insert the following into your page:
-//
-//    <script id=threecents src=http://cdn.elevenbasetwo.com/threecents.js data-key=208eb5ed1aed5f8111d023bf718ba3ff></script>
-//
-// making sure to replace the `data-key` value with your actual Threecents key.
-
 (function() {
   var tc = document.getElementById('threecents');
   var node = document.createElement('div');
-  node.innerHTML = '<form id=threecent-form><textarea></textarea><button id=threecents-button>Submit</button></form>';
+  node.innerHTML = '<form><textarea id=tcText></textarea><button id=tcButton>Submit</button></form>';
   tc.parentNode.insertBefore(node, tc.nextSibling);
 
-  var button = document.getElementById('threecents-button');
-  button.onclick = function() {
+  document.getElementById('tcButton').onclick = function() {
     var client = new XMLHttpRequest();
-    var key = tc.getAttribute("data-key");
-    client.open("POST", "http://threecents.elevenbasetwo.com/api/feedback/" + key, false);
-    client.send(JSON.stringify({feedback: "It's the best thing ever!"}));
+    client.open("POST", "http://threecents.elevenbasetwo.com/api/feedback/" + tc.getAttribute("data-key"), false);
+    client.send(JSON.stringify({feedback: document.getElementById('tcText').value}));
   }
 })();
